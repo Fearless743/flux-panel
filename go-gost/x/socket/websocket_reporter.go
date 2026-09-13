4f32103e337db52493e100ae7c11112ee149c074
Fearless <admin@fearless.icu>
2026-09-13 08:48:52 +0800

    fix: 修复 main.go 和 websocket_reporter.go 函数签名匹配问题

--- go-gost/x/socket/websocket_reporter.go
+++ go-gost/x/socket/websocket_reporter.go
@@ -1105,7 +1105,7 @@ func getMemoryInfo() MemoryInfo {
 }
 
 // StartWebSocketReporterWithConfig 使用配置字段启动WebSocket报告器
-func StartWebSocketReporterWithConfig(addr string, secret string, http int, tls int, socks int, ssl bool, version string, supportBrutal bool) *WebSocketReporter {
+func StartWebSocketReporterWithConfig(addr string, secret string, http int, tls int, socks int, ssl bool, version string) *WebSocketReporter {
 
 	// 自动检测系统是否支持 TCP Brutal
 	supportBrutal := CheckBrutalSupport()
